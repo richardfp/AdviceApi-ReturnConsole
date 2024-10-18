3d343fe11a507dcdb015753b98a2b8aa15529c38
richardfp <ri_chardfilipe@hotmail.com>
2024-10-18 17:28:01 -0300

    comitado

---
+++ Advice-Api-ConsoleReturn/Program.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using Flurl.Http;
+
+var apiReturns = await "https://api.adviceslip.com/advice"
+            .GetJsonAsync<ApiReturn>();
+
+Console.WriteLine($"slip: {apiReturns.slip.slip_id}, mensagem: {apiReturns.slip.advice}");
+
+
+
+public class ApiReturn
+{
+    public Slip slip { get; set; }
+}
+
+public class Slip
+{
+    public int slip_id { get; set; }
+    public string advice { get; set; }
+}
